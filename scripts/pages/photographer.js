7de21c832fc59dd20204ea5c9549490854ee394d
ahmed sfaxi <nijushihosho@gmail.com>
2022-10-02 23:07:19 +0200

    test fetch data

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,15 @@
 //Mettre le code JavaScript lié à la page photographer.html
+et photographersArray = [];
+  async function fetchPhotographers() {
+    await fetch("./data/photographers.json")
+      .then((res) => res.json())
+      // eslint-disable-next-line no-return-assign
+      .then((data) => (photographersArray = data.photographers));
+    console.log(photographersArray);
+  }
+
+  fetchPhotographers();
+
+
+  const photographers = photographersArray;
+  console.log(photographers);
